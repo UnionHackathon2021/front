0ac93952d8c72eb024b75780b2e16f9448333f4e
Kimjimin0807 <jm004471@naver.com>
2021-11-25 01:48:35 +0900

    [ADD] asset 추가

---
+++ src/asset/index.ts
@@ -0,0 +1,5 @@
+export { default as Logo } from "./header/Logo.svg";
+export { default as Background } from "./background/Background.svg";
+export { default as ProjectName } from "./header/ProjectName.svg";
+export { default as NegativeIcon } from "./main/NegativeIcon.svg";
+export { default as PositiveIcon } from "./main/PositiveIcon.svg";
