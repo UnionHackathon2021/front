57b98c79171c127193635660561b6bcd9391da98
Kimjimin0807 <jm004471@naver.com>
2021-11-25 01:48:10 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainRouter from "./route/MainRouter";
+import GlobalStyle from "./style/globalStyle";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <MainRouter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/style/index.ts
@@ -0,0 +1,3 @@
+export const color = {
+  main: "#FF7777",
+};
