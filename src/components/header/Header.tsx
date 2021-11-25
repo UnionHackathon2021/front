7dda398379b58dc10f042d0930ba1d9da5b0a4d3
Kimjimin0807 <jm004471@naver.com>
2021-11-26 00:30:06 +0900

    [ADD] 경로 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import { Link } from "react-router-dom";
 import { Logo, ProjectName } from "../../asset";
 import * as S from "./style";
 
@@ -6,8 +7,12 @@ const Header: FC = () => {
   return (
     <>
       <S.HeaderWrapper>
-        <img src={Logo} alt="로고" className="logo" />
-        <img src={ProjectName} alt="프로젝트 이름" />
+        <Link to="/">
+          <img src={Logo} alt="로고" className="logo" />
+        </Link>
+        <Link to="/">
+          <img src={ProjectName} alt="프로젝트 이름" />
+        </Link>
       </S.HeaderWrapper>
     </>
   );
